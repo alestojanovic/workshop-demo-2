467b996aaf8ec2c6648820f4f2b33514edc31297
aleksandar.stojanovic.ext <aleksandar.stojanovic.ext@adevinta.com>
2024-09-11 13:27:01 +0200

    feat: add mui

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,6 +3,7 @@ import logo from '../assets/logo.svg'
 import {useEffect, useState} from 'react'
 import {Favorites} from '../components/Favorites'
 import {toast} from 'react-toastify'
+import {Button} from '@mui/material'
 
 export const Homepage = () => {
   const navigate = useNavigate()
@@ -24,7 +25,10 @@ export const Homepage = () => {
       <div className='favorite-component'>
         <Favorites favoriteStatus={like} favoriteAction={handleFavor} />
       </div>
-
+      <button className='custom-button'>Button</button>
+      <Button variant='outlined' color='success'>
+        Contained
+      </Button>
       {like ? (
         <div onClick={() => navigate('/About')} data-testid='reactLogo'>
           <img src={logo} className='App-logo' alt='logo' />
